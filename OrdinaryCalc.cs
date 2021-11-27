8ad89ecc6c9c9b4c75fc6ce2274e38e6ffb831c3
Sergey <sabserj2017@gmail.com>
2021-11-27 15:09:06 +0200

    OrdinaryCalc

--- OrdinaryCalc.cs
+++ OrdinaryCalc.cs
@@ -8,13 +8,22 @@ namespace ConsoleApp2
 {
     abstract class Calc
     {
+        public string Make { get => GetType().Name; }
         public abstract double Add(double a, double b);
         public abstract double Sub(double a, double b);
-        public double Mul(double a, double b) { return a * b; }
-        public double Div(double a, double b) { return a / b; }
-    }
-    class OrdinaryCalc
-    {
+        public abstract double Mul(double a, double b);
+        public abstract double Div(double a, double b);
 
+        //public double Add(double a, double b) => a + b;
+        //public double Sub(double a, double b) => a - b;
+        //public double Mul(double a, double b) => a * b;
+        //public double Div(double a, double b) => a / b;
+    }
+    class OrdinaryCalc : Calc
+    {
+        public override double Add(double a, double b) => a + b;
+        public override double Sub(double a, double b) => a - b;
+        public override double Mul(double a, double b) => a + b;
+        public override double Div(double a, double b) => a - b;
     }
 }
