ee407b7d898ba743f3f28b669ae3cd136e66759d
viacheslav177 <@DESKTOP-8R353C7>
2021-11-27 17:35:12 +0300

    OrdCalc in main

--- Program.cs
+++ Program.cs
@@ -11,10 +11,46 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("=============== Calculator ===============");
-            while (true)
+            var cycle = true;
+            var ordcalc = new OrdinaryCalc();
+            var advcalsc = new AdvancedCalc();
+            while (cycle)
             {
-                switch ()
+                switch (Input("1 - Ordinary calc\n2 - Advanced calc\nOhter to exit: "))
                 {
+                    case "1":
+                        var action = Input("Choose the action (+,=,*,/)");
+                        double a, b;
+                        try
+                        {
+                            a = Convert.ToDouble(Input("Enter the first number"));
+                            b = Convert.ToDouble(Input("Enter the second number"));
+                        }
+                        catch { Console.WriteLine("Numbers was entred incorrectly"); }
+                        switch (action) 
+                        {
+                            case "+":
+                                Console.WriteLine($"Asnwer: {ordcalc.Add(a, b)}");
+                                break;
+                            case "-":
+                                Console.WriteLine($"Asnwer: {ordcalc.Sub(a, b)}");
+                                break;
+                            case "*":
+                                Console.WriteLine($"Asnwer: {ordcalc.Mul(a, b)}");
+                                break;
+                            case "/":
+                                Console.WriteLine($"Asnwer: {ordcalc.Div(a, b)}");
+                                break;
+                            default:
+                                Console.WriteLine("The action was chosen incorrectly");
+                                break;
+                        }
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        cycle = false;
+                        break;
                 }
             }
         }
